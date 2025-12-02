c4e30fc14ddf27b3db03c27982859d2fdf18b33d
Your Name <=>
2025-12-02 23:34:10 +0200

    done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500 bg-red-300">
-        Welcome to Nativewind!
+      <Text className="text-5xl font-bold text-secondary ">
+       saadon 
       </Text>
+
     </View>
   );
 }
